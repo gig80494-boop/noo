4a5220aefd7c5e3ba587deaa65f8238e45f7523b
gig80494-boop <gig80494@gmail.com>
2026-09-03 17:59:03 +0400

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ client.on('messageCreate', async (message) => {
     }
 
     // 2. إزالة شخص من النوباك
-    if (action === 'remove') {
+    if (action === 'removed' || action === 'remove') {
       const userId = args[2];
 
       if (!/^\d+$/.test(userId || '')) {
